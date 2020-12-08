46e71d9b4e4fbffe58e300304ece6908dcec0eed
TimoHarting <45542135+TimoHarting@users.noreply.github.com>
2020-12-08 14:59:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Wieviele Spiele möchtest du einräumen?");
+            int eingabe = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 1; i <= eingabe; i++)
+            {
+                Console.WriteLine("Welches Spiel möchtest du deiner Spielesammlung hinzufügen");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Wann ist das Spiel herausgekommen");
+                DateTime dt = Convert.ToDateTime(Console.ReadLine());
+
+                Spiel s = new Spiel(name, dt);
+                
+            }
+
+            Spiel.GibListeAus();
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Spiel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    public class Spiel
+    {
+        public string Name;
+        public DateTime Datum;
+        private static List<Spiel> spiele = new List<Spiel>();
+
+        public Spiel(string name, DateTime datum)
+        {
+            this.Name = name;
+            this.Datum = datum;
+            spiele.Add(this);
+        }
+
+        public static void GibListeAus()
+        {
+            foreach (Spiel s in spiele)
+            {
+                Console.WriteLine(s.Name);
+                Console.WriteLine(s.Datum);
+            }
+        }
+
+
+    }
+}
